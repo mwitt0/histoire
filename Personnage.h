afa70f43aa5413a8b74afad3879ecf912b50d3f8
mwitt0 <60476083+mwitt0@users.noreply.github.com>
2021-11-30 21:38:03 +0100

    Update Personnage.h
    add recevoirDegats, infligerDegats

--- Personnage.h
+++ Personnage.h
@@ -16,6 +16,8 @@ public:
     int type;
     Personnage(std::string nom,int type,Equipement* arme,Equipement* armure);
     Personnage(const char string[8]);
+    void recevoirDegats(int PA);
+    void infligerDegats(Personnage &cible, int PA);
 };
 
 

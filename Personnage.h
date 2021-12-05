428fce96b28997728ca99a756d90a0346de9e614
mwitt0 <60476083+mwitt0@users.noreply.github.com>
2021-12-05 16:45:31 +0100

    Update Personnage.h
    Ajouté recevoirDegats, infligerDegats
    PV et nom dans protected

--- Personnage.h
+++ Personnage.h
@@ -10,15 +10,22 @@
 class Personnage {
 
 public:
+    Personnage();
+    void recevoirDegats(int degats);
+    void infligerDegats(Personnage &cible, int degats) const;
+    
     Equipement* arme;
     Equipement* armure;
-    std::string nom;
-    int PV;
+
     int type;
     int getPV();
     void setPV(int);
     Personnage(std::string nom,int type,Equipement* arme,Equipement* armure,int PV);
     Personnage(const char string[8]);
+    
+protected:
+    int PV;
+    std::string nom;
 };
 
 
